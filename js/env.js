d7ad3604737faad35a1572cd4563bf4faf066b20
robertoloco <robertoalmela300@gmail.com>
2025-04-17 20:05:21 +0000

    Deploying to main from @ robertoloco/Queveohoy@5b191217c5782205c0ca09e95860140f9fc7c2f3 🚀

---
+++ js/env.js
@@ -0,0 +1,5 @@
+// API Keys configuration
+window.TMDB_API_KEY = '6f8a752ff9858fade9e122cbe6896b63';
+window.GEMINI_API_KEY = 'AIzaSyBH4mXio_L0JAyoJMbSHG5twq0KrbxS7v4';
+
+console.log('🔑 API keys cargadas correctamente');
